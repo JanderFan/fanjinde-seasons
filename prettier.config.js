78a3f2d57e74f35cb8c0aa684564870f9e30b816
JanderFan <2928528663@qq.com>
2026-06-04 16:18:55 +0800

    1

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  quoteProps: 'as-needed',
+  singleAttributePerLine: true,
+
+};
